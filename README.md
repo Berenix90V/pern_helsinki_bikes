298c119bc4232a26408926bf5b115b636e0d797d
Veronica Zanella <zanella.veronica@outlook.it>
2022-09-16 16:10:18 +0300

    deleted previous version: keep 1 column for date and time in right format and changed the names of the columns

--- README.md
+++ README.md
@@ -5,7 +5,12 @@ Their names are:
 - 2021-05.csv, 
 - 2021-06.csv, 
 - 2021-07.csv, 
-- stations,csv
+- stations.csv
 
 Open psql and enter in localhost, database postgres, default port, with the default user postgres and create the database helsinki_bikes with the command:
 `CREATE DATABASE helsinki_bikes;`
+
+## Data validation
+Before executing the python script install `pandas` and `pandera` with one of the following commands:
+- `py -m pip install pandera`  
+- `pip install pandera`  
--- fetch_data/fix_data.py
+++ fetch_data/fix_data.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import pandera as pa
+from pandera import Column, Check
 
 file_path = 'fetch_data/data/originals/'
 output_path = 'fetch_data/data/definitive/'
@@ -6,27 +8,29 @@ output_path = 'fetch_data/data/definitive/'
 stations_file = 'stations.csv'
 trips_files = ['2021-05.csv']
 
+# validation schema
+
+
 for file_name in trips_files:
     # Read csv file
     df = pd.read_csv(file_path + file_name)
 
-    # separate date and time in departure and in return
-    df[['Departure_date','Departure_time']] = df['Departure'].str.split('T', expand=True)
-    df.drop(['Departure'], axis=1, inplace=True)
+    # Rename column with datetime
+    df.rename(columns = {'Departure':'Departure_datetime', 'Return':'Return_datetime'}, inplace = True)
 
-    df[['Return_date','Return_time']] = df['Return'].str.split('T', expand=True)
-    df.drop(['Return'], axis=1, inplace=True)
-
-    # sort the columns in the right order (departure and return dates and time for first)
-    cols = df.columns.tolist()
-    cols = cols[-4:] + cols[:-4]
-    df = df[cols]
+    # Conversion from string to date
+    df['Departure_datetime'] = df['Departure_datetime'].str.replace('T', ' ')
+    df['Return_datetime'] = df['Return_datetime'].str.replace('T', ' ')
+    df['Departure_datetime'] = pd.to_datetime(df['Departure_datetime'], format='%Y-%m-%d %H:%M:%S')
+    df['Return_datetime'] = pd.to_datetime(df['Return_datetime'], format='%Y-%m-%d %H:%M:%S')
 
     # Replace all spaces in column names with _
     df.columns = df.columns.str.replace(' ', '_')
 
     # delete rows with null values
     df = df.dropna()
+
+
 
     print(df.dtypes)
 
