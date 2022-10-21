bca39f3927eeec2ff208ef2f907f4e1c0b821ee7
Veronica Zanella <zanella.veronica@outlook.it>
2022-10-21 16:10:07 +0300

    test fetchAll methos of class Station

--- server/src/__tests__/station.test.js
+++ server/src/__tests__/station.test.js
@@ -1,7 +1,17 @@
+import {Station} from "../models/Station";
+import {sequelize} from "../db/connection";
+import {QueryTypes} from "sequelize";
 
+beforeEach(async ()=>{
+    await sequelize.authenticate()
+})
+afterEach(async()=>{
+    await sequelize.close()
+})
 
-describe("Server.ts test", ()=>{
-    test("Math test", ()=>{
-        expect(2+2).toBe(4)
+describe("Station", ()=>{
+    it("Should return all the stations in the database", async()=>{
+        const count = await sequelize.query("SELECT count(*) from stations", {type: QueryTypes.SELECT})
+        await expect(Station.fetchAll()).resolves.toHaveLength(+count[0].count)
     })
 })
